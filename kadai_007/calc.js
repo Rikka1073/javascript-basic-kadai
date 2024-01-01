f937f17f49c2473d8dbc693d8481b28900543a94
Rikka1073 <145694308+Rikka1073@users.noreply.github.com>
2024-01-01 13:31:49 +0900

    calc.jsの修正

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -4,7 +4,6 @@ let num = Math.floor(Math.random() * 21);
 // 変数numの値を出力する（確認用）
 console.log(num);
 
-// 変数numの値が3の倍数であれば、「3の倍数です」という文字列を出力する
 // 変数numの値が3と5倍数であれば、「3と5の倍数です」という文字列を出力する
 if (num % 3 === 0 && num % 5 === 0) {
   console.log('3と5の倍数です');
